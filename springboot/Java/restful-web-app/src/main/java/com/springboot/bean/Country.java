3283298904f16bc6bc10052bc373054c1f496e10
nimeshsilva1997 <nimeshsilva@gmail.com>
2020-05-20 12:50:33 -0400

    Next Commit.

--- springboot/Java/restful-web-app/src/main/java/com/springboot/bean/Country.java
+++ springboot/Java/restful-web-app/src/main/java/com/springboot/bean/Country.java
@@ -1,4 +1,27 @@
 package com.springboot.bean;
 
 public class Country {
+
+    private String name;
+    private int population;
+
+    public Country() {
+    }
+
+    public Country(String name, int population) {
+        this.name = name;
+        this.population = population;
+    }
+    //method gets the name
+    public String getName() {
+        return name;
+    }
+    //method gets the population
+    public int getPopulation() {
+        return population;
+    }
+    //method sets the population
+    public void setPopulation(int population) {
+        this.population = population;
+    }
 }
