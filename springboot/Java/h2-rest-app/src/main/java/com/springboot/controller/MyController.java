d15c4a77467c7a4eff8568f820aee9bef0989b78
nimeshsilva1997 <nimeshsilva@gmail.com>
2020-05-20 15:22:33 -0400

    Next Commit.

--- springboot/Java/h2-rest-app/src/main/java/com/springboot/controller/MyController.java
+++ springboot/Java/h2-rest-app/src/main/java/com/springboot/controller/MyController.java
@@ -1,4 +1,29 @@
 package com.springboot.controller;
+//controller class for the Spring Boot RESTful application
+import com.springboot.bean.City;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import com.springboot.service.ICityService;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController //creates a RESTful controller
 public class MyController {
+
+    @Autowired
+    //inject a ICityService into the countryService field
+    private ICityService cityService;
+
+    @RequestMapping("/cities")//used to map web requests to Spring controller methods
+    public List<City> findCities() {
+
+        return cityService.findAll();
+    }
+
+    @RequestMapping("/cities/{userId}")
+    public City findCity(@PathVariable Long userId) {
+
+        return cityService.findById(userId);
+    }
 }
