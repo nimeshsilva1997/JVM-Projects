9d58fabbb5a9020cb9aab8ea10d585bbb607d800
nimeshsilva1997 <nimeshsilva@gmail.com>
2020-05-20 12:53:01 -0400

    Next Commit.

--- springboot/Java/restful-web-app/src/main/java/com/springboot/service/ICountryService.java
+++ springboot/Java/restful-web-app/src/main/java/com/springboot/service/ICountryService.java
@@ -1,4 +1,9 @@
 package com.springboot.service;
+//fields from the countries.csv file are mapped to the Country class
+import com.springboot.bean.Country;
+import java.util.ArrayList;
 
-public class ICountryService {
+public interface ICountryService {
+    //This is the ICountryService interface. It contains one method called findAll()
+    public ArrayList<Country> findAll();
 }
