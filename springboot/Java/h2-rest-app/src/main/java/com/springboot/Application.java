e8137dcf2ac08832e9bb5ef61aaa964f6e25f9bb
nimeshsilva1997 <nimeshsilva@gmail.com>
2020-05-20 15:24:44 -0400

    Next Commit.

---
+++ springboot/Java/h2-rest-app/src/main/java/com/springboot/Application.java
@@ -0,0 +1,12 @@
+package com.springboot;
+//Application sets up the Spring Boot application
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication //enables auto-configuration and component scanning
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
