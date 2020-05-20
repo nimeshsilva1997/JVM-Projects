7a17b8cc39f46adcf5befe1e9e547f29ffec23f9
nimeshsilva1997 <nimeshsilva@gmail.com>
2020-05-20 15:12:47 -0400

    Next Commit.

--- springboot/Java/h2-rest-app/src/main/java/com/springboot/service/ICityService.java
+++ springboot/Java/h2-rest-app/src/main/java/com/springboot/service/ICityService.java
@@ -1,4 +1,10 @@
 package com.springboot.service;
 
+import com.springboot.bean.City;
+import java.util.List;
+//provides contract methods to get all cities and get a city by its id from the data source
 public interface ICityService {
+
+    public List<City> findAll();
+    public City findById(Long id);
 }
