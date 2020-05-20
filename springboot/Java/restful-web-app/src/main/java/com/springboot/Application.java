53cc006ea0c4a66a2825f48b93337f467a73ac74
nimeshsilva1997 <nimeshsilva@gmail.com>
2020-05-20 13:37:03 -0400

    Next Commit.

--- springboot/Java/restful-web-app/src/main/java/com/springboot/Application.java
+++ springboot/Java/restful-web-app/src/main/java/com/springboot/Application.java
@@ -1,4 +1,11 @@
 package com.springboot;
+//Application class sets up the Spring Boot application
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication //enables auto-configuration and component scanning
 public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 }
--- springboot/Java/restful-web-app/src/main/java/com/springboot/controller/MyController.java
+++ springboot/Java/restful-web-app/src/main/java/com/springboot/controller/MyController.java
@@ -4,7 +4,9 @@ import com.springboot.bean.Country;
 import com.springboot.service.ICountryService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController//creates a RESTful controller
@@ -13,7 +15,7 @@ public class MyController {
     @Autowired
     private ICountryService countryService;
 //inject a CountryService into the countryService variable
-    @RequestMapping("/countries") //used to map web requests to Spring controller methods
+    @RequestMapping(value = "/countries", method = RequestMethod.GET, produces = MediaType.APPLICATION_XML_VALUE) //used to map web requests to Spring controller methods. Tells the controller to return XML data
     public List<Country> listCountries() {
         return  countryService.findAll();
     }
