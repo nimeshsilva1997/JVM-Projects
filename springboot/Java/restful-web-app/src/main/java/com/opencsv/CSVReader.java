83ef9bdf07cef967eab1012e4cebb67250762065
nimeshsilva1997 <nimeshsilva@gmail.com>
2020-05-20 13:39:59 -0400

    Next Commit.

--- springboot/Java/restful-web-app/src/main/java/com/opencsv/CSVReader.java
+++ springboot/Java/restful-web-app/src/main/java/com/opencsv/CSVReader.java
@@ -1,4 +1,12 @@
 package com.opencsv;
 
+import java.io.InputStreamReader;
+
 public class CSVReader {
+    public CSVReader(InputStreamReader inputStreamReader) {
+    }
+
+    public String[] readNext() {
+        return null;
+    }
 }
