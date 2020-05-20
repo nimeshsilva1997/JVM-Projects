a59b3326ece6e5dfa563e2813ebd521550a2f8e4
nimeshsilva1997 <nimeshsilva@gmail.com>
2020-05-20 13:59:15 -0400

    First Commit.

---
+++ springboot/Java/h2-rest-app/src/main/java/com/springboot/h2restapp/H2RestAppApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.h2restapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H2RestAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(H2RestAppApplication.class, args);
+    }
+
+}
---
+++ springboot/Java/h2-rest-app/src/test/java/com/springboot/h2restapp/H2RestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.h2restapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class H2RestAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
