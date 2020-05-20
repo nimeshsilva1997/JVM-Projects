ce0e498af8778a76db691fbc970dc8e0d6f68103
nimeshsilva1997 <nimeshsilva@gmail.com>
2020-05-19 22:09:37 -0400

    Next commit.

--- Java/springbootsubmissionform/src/main/java/com/springboot/controller/MyController.java
+++ Java/springbootsubmissionform/src/main/java/com/springboot/controller/MyController.java
@@ -1,5 +1,19 @@
 package com.springboot.controller;
 
+import com.springboot.bean.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+@Controller //sets Controller to control and read the MyController class
 public class MyController {
+
+    @GetMapping("/addUser") //sets up getting the mapping to add user
+    public String sendForm (User user) {
+        return "addUser";
+    }
+    @PostMapping("/addUser") //sets up posting the mapping to show a message
+    public String processForm (User user) {
+        return "showMessage";
+    }
 }
